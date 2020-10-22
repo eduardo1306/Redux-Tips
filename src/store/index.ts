641fce7e6a4fbe4cdf14677270d3e3ead064673b
Eduardo Ferreira <eduardoferreira@icloud.com>
2020-10-22 00:16:25 -0300

    Redux course has been finished

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,27 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(() => []);
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+
+import { ICartState } from './modules/cart/types';
+
+export interface IState {
+  cart: ICartState;
+}
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(...middlewares),
+  ),
+);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
